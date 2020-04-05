ed2d9e3b4f06a962356da3a574b7fdd38979fd71
trilltown <phil.awich@gmail.com>
2020-04-05 15:26:25 -0400

    Add createStore

--- redux-two-lab/src/App.js
+++ redux-two-lab/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import TodoInput from './components/toDoInput';
+import TodoList from './components/toDoList';
 
 function App() {
   return (
     <div className="App">
       Hello World
+    <TodoInput/>
+    <TodoList/>
     </div>
   );
 }
