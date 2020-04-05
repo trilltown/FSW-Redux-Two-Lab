dc69daf44943e7f113a966e45b6b34d7975a0d30
trilltown <phil.awich@gmail.com>
2020-04-05 14:59:15 -0400

    Create Todo Schema

---
+++ redux-two-lab/src/reducers/toDoReducer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const initialState = {
+    todos: [
+        {
+            id: 1,
+            text: "Buy groceries",
+            completed: true
+        },
+        {
+            id: 2,
+            text: "Do Laundry",
+            completed: false
+        }
+    ]
+};
+
+const toDoReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    const newState = { ...state };
+    switch (action.type) {
+        case 
+    }
+}
