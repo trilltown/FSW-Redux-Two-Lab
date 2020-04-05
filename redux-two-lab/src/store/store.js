bcbbb744c49075fe5d446914a47024fe0d3cf3b4
trilltown <phil.awich@gmail.com>
2020-04-05 15:32:12 -0400

    Add rootReducer and update store

--- redux-two-lab/src/store/store.js
+++ redux-two-lab/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 
-import rootReducer from '../reducers/toDoReducer';
+import rootReducer from '../reducers/rootReducer.js';
 
 const store = createStore(rootReducer)
 
